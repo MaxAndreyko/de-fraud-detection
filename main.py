3de50179810c35cc6f4699fdc14933b5d7874f2b
MaxAndreyko <max_andreyko@mail.ru>
2024-12-25 00:51:19 +0300

    feat: add pipeline that creates schema and loads data to staging tables

---
+++ main.py
@@ -0,0 +1,49 @@
+import os
+from dotenv import load_dotenv, find_dotenv
+import yaml
+
+from src.os.read import get_incoming_data, prep_incoming_data
+from src.database.models import DWHSchema, BankSchema
+from src.database.clients import DWHClient, BankDBClient
+
+if __name__ == "__main__":
+    load_dotenv(find_dotenv())
+
+    with open("configs/os/files.yaml", "r") as os_cfg:
+        os_cfg = yaml.safe_load(os_cfg)
+
+    # Establish connection with bank database
+    bank_schema = BankSchema.from_yaml("configs/database/bank.yaml")
+    bank_client = BankDBClient(
+        database=os.getenv("DB_NAME"),
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASS"),
+        port=os.getenv("DB_PORT"),
+        schema=bank_schema
+    )
+
+    # Establish connection with data warehouse
+    dwh_schema = DWHSchema.from_yaml("configs/database/dwh.yaml")
+    dwh_client = DWHClient(
+        database=os.getenv("DB_NAME"),
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASS"),
+        port=os.getenv("DB_PORT"),
+        schema=dwh_schema
+    )
+
+    # Initialize data warehouse schema
+    dwh_client.create_schema("pattern.ddl")
+
+    # Insert bank data to tables
+    dwh_client.insert_bank_tables(bank_client)
+
+    # Get incoming data
+    incoming_data = get_incoming_data(os_cfg["data_dir"], os_cfg["patterns"])
+
+    incoming_data = prep_incoming_data(incoming_data, os_cfg["preprocess"])
+
+    # Insert incoming data to tables
+    dwh_client.insert_incoming_tables(incoming_data)
